2ed0ea2124e5f73454ca10f32fc701d02157ad26
lakshmi <lakshmisswetha@gmail.com>
2023-11-09 23:53:19 +0530

    async-await added

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,31 +1,69 @@
 const todoModel = require("../models/todoModel");
 
+// module.exports.getTodo = async (req, res) => {
+//   const todo = await todoModel.find();
+//   res.send(todo);
+// };
 module.exports.getTodo = async (req, res) => {
-  const todo = await todoModel.find();
-  res.send(todo);
+  try {
+    const todo = await todoModel.find();
+    res.send(todo);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+// module.exports.saveTodo = async (req, res) => {
+//   const { text } = req.body;
+//   todoModel.create({ text }).then((data) => {
+//     console.log("Added Successfully...");
+//     res.status(201);
+//     res.send(data);
+//   });
+// };
 module.exports.saveTodo = async (req, res) => {
-  const { text } = req.body;
-  todoModel.create({ text }).then((data) => {
+  try {
+    const { text } = req.body;
+    const data = await todoModel.create({ text });
+
     console.log("Added Successfully...");
-    res.status(201);
-    res.send(data);
-  });
+    res.status(201).send(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+// module.exports.updateTodo = async (req, res) => {
+//   const { _id, text } = req.body;
+//   todoModel
+//     .findByIdAndUpdate(_id, { text })
+//     .then(() => res.send("Updated Successfully..."))
+//     .catch((err) => console.log(err));
+// };
 module.exports.updateTodo = async (req, res) => {
-  const { _id, text } = req.body;
-  todoModel
-    .findByIdAndUpdate(_id, { text })
-    .then(() => res.send("Updated Successfully..."))
-    .catch((err) => console.log(err));
+  try {
+    const { _id, text } = req.body;
+    await todoModel.findByIdAndUpdate(_id, { text });
+    res.status(200).send("Updated Successfully...");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
+// module.exports.deleteTodo = async (req, res) => {
+//   const { _id } = req.body;
+//   todoModel
+//     .findByIdAndDelete(_id)
+//     .then(() => res.send("Deleted Successfully..."))
+//     .catch((err) => console.log(err));
+// };
 module.exports.deleteTodo = async (req, res) => {
-  const { _id } = req.body;
-  todoModel
-    .findByIdAndDelete(_id)
-    .then(() => res.send("Deleted Successfully..."))
-    .catch((err) => console.log(err));
+  try {
+    const { _id } = req.body;
+    await todoModel.findByIdAndDelete(_id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
--- frontend/src/utils/HandleApi.js
+++ frontend/src/utils/HandleApi.js
@@ -2,43 +2,101 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:5000";
 
-const getAllTodo = (setTodo) => {
-  axios
-    .get(baseUrl)
-    .then(({ data }) => {
-      setTodo(data);
-    })
-    .catch((err) => console.log(err));
+// const getAllTodo = (setTodo) => {
+//   axios
+//     .get(baseUrl)
+//     .then(({ data }) => {
+//       setTodo(data);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+const getAllTodo = async (setTodo) => {
+  try {
+    const data = await fetch(baseUrl);
+    const json = await data.json();
+    setTodo(json);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const addTodo = (text, setText, setTodo) => {
-  axios
-    .post(`${baseUrl}/save`, { text })
-    .then((data) => {
-      setText("");
-      getAllTodo(setTodo);
-    })
-    .catch((err) => console.log(err));
+// const addTodo = (text, setText, setTodo) => {
+//   axios
+//     .post(`${baseUrl}/save`, { text })
+//     .then((data) => {
+//       setText("");
+//       getAllTodo(setTodo);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+const addTodo = async (text, setText, setTodo) => {
+  try {
+    const data = await fetch(`${baseUrl}/save`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    });
+    setText("");
+    await getAllTodo(setTodo);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const updateTodo = (todoId, text, setText, setTodo, setIsUpdating) => {
-  axios
-    .post(`${baseUrl}/update`, { _id: todoId, text })
-    .then((data) => {
-      setText("");
-      setIsUpdating(false);
-      getAllTodo(setTodo);
-    })
-    .catch((err) => console.log(err));
+// const updateTodo = (todoId, text, setText, setTodo, setIsUpdating) => {
+//   axios
+//     .post(`${baseUrl}/update`, { _id: todoId, text })
+//     .then((data) => {
+//       setText("");
+//       setIsUpdating(false);
+//       getAllTodo(setTodo);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+const updateTodo = async (todoId, text, setText, setTodo, setIsUpdating) => {
+  try {
+    const data = await fetch(`${baseUrl}/update`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ _id: todoId, text }),
+    });
+    setText("");
+    setIsUpdating(false);
+    await getAllTodo(setTodo);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const deleteTodo = (_id, setTodo) => {
-  axios
-    .post(`${baseUrl}/delete`, { _id })
-    .then((data) => {
-      getAllTodo(setTodo);
-    })
-    .catch((err) => console.log(err));
+// const deleteTodo = (_id, setTodo) => {
+//   axios
+//     .post(`${baseUrl}/delete`, { _id })
+//     .then((data) => {
+//       getAllTodo(setTodo);
+//     })
+//     .catch((err) => console.log(err));
+// };
+
+const deleteTodo = async (_id, setTodo) => {
+  try {
+    const data = await fetch(`${baseUrl}/delete`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ _id }),
+    });
+    getAllTodo(setTodo);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export { getAllTodo, addTodo, updateTodo, deleteTodo };
