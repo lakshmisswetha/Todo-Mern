ce18e68e6c89c4027311b6fdd80c6b0a3fc76e2f
lakshmi <lakshmisswetha@gmail.com>
2023-11-27 00:06:40 +0530

    login-signup routes created

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,11 @@
 import { TextField, ThemeProvider, Checkbox, Button } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import coverImage from "../assets/login-cover.png";
+import coverImage from "../assets/login-img.jpg";
 
 const theme = {
     palette: {
         primary: {
-            main: "#ffff",
+            main: "#000000",
         },
     },
 };
@@ -15,40 +15,45 @@ const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Login = () => {
     return (
-        <div className=" h-screen bg-slate-300 flex justify-between">
-            <img className="w-[55%] h-[100vh]" src={coverImage} alt="login" />
+        <div className=" h-screen bg-white-50 flex justify-between">
+            <div className="p-[100px]">
+                <img className=" w-[100%] h-[100%]" src={coverImage} alt="login" />
+            </div>
 
-            <div className="w-[45%] bg-gray-900 flex flex-col justify-around">
-                <div className="flex flex-col text-center text-white">
-                    <div className=" text-4xl ">Welcome Back !</div>
-                    <div className="mt-6">Please enter your details</div>
+            <div className="w-[50%] rounded-3xl bg-gradient-to-br from-gray-200 from-40% to-indigo-200 flex flex-col justify-around my-7 mr-5">
+                <div className="flex flex-col text-center text-black">
+                    <div className=" text-4xl font-bold">Welcome Back!!</div>
+                    <div className="mt-6 font-semibold">Register your account</div>
                 </div>
 
                 <ThemeProvider theme={themes}>
-                    <div className="flex flex-col mx-[120px]">
+                    <form className="flex flex-col mx-[180px]">
                         <TextField
                             // InputProps={{ inputLabelProps: { style: { color: "white" } } }}
                             id="standard-basic"
                             label="Email"
                             variant="standard"
+                            style={{ marginBottom: "10px" }}
                         />
                         <TextField id="standard-basic" label="Password" variant="standard" />
                         <div className="flex justify-between items-center mt-2">
                             <div className="flex items-center ">
                                 <Checkbox {...label} defaultChecked />
-                                <div className="text-white">Remember for 30 days</div>
+                                <div className="text-black">Remember for 30 days</div>
                             </div>
-                            <div className="forgot-pswd text-white items-center ">
+                            <div className="forgot-pswd text-black items-center ">
                                 Forgot Password?
                             </div>
                         </div>
                         <Button sx={{ margin: " 30px 100px" }} variant="contained">
-                            Log In
+                            Sign IN
                         </Button>
-                    </div>
+                    </form>
                 </ThemeProvider>
 
-                <div className="text-white text-center">Don't have an account? SIGN UP</div>
+                <div className="text-blue-900 font-semibold text-center">
+                    Don't have an account? SIGN UP
+                </div>
             </div>
         </div>
     );
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { TextField, ThemeProvider, Checkbox, Button } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import coverImage from "../assets/login-img.jpg";
+
+const theme = {
+    palette: {
+        primary: {
+            main: "#000000",
+        },
+    },
+};
+
+const themes = createTheme(theme);
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Signup = () => {
-    return <div>SignUP</div>;
+    return (
+        <div className=" h-screen bg-white-50 flex justify-between">
+            <div className="p-[100px]">
+                <img className=" w-[100%] h-[100%]" src={coverImage} alt="login" />
+            </div>
+
+            <div className="w-[50%] rounded-3xl bg-gradient-to-br from-gray-200 from-40% to-indigo-200 flex flex-col justify-around my-7 mr-5">
+                <div className="flex flex-col text-center text-black">
+                    <div className=" text-4xl font-bold">Welcome !!</div>
+                    <div className="mt-6 font-semibold">Register your account</div>
+                </div>
+
+                <ThemeProvider theme={themes}>
+                    <form className="flex flex-col mx-[180px]">
+                        <TextField
+                            id="standard-basic"
+                            label="Username"
+                            variant="standard"
+                            style={{ marginBottom: "10px" }}
+                        />
+                        <TextField
+                            // InputProps={{ inputLabelProps: { style: { color: "white" } } }}
+                            id="standard-basic"
+                            label="Email"
+                            variant="standard"
+                            style={{ marginBottom: "10px" }}
+                        />
+                        <TextField id="standard-basic" label="Password" variant="standard" />
+                        <div className="flex justify-between items-center mt-2">
+                            <div className="flex items-center ">
+                                <Checkbox {...label} defaultChecked />
+                                <div className="text-black">
+                                    I agree with terms and privacy policy
+                                </div>
+                            </div>
+                            {/* <div className="forgot-pswd text-white items-center ">
+                                Forgot Password?
+                            </div> */}
+                        </div>
+                        <Button sx={{ margin: " 30px 100px" }} variant="contained">
+                            Create Account
+                        </Button>
+                    </form>
+                </ThemeProvider>
+
+                <div className="text-blue-900 font-semibold text-center">
+                    Already have an account? SIGN IN
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Signup;
